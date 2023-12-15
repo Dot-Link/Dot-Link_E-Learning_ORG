91a16642e202be260d2a5b761f0c7f4dac257b56
Dot-Link <dotlinked101@gmail.com>
2023-12-15 21:42:51 +0800

    E-leanring-ORG-main

---
+++ assests/css/js/videohome.js
@@ -0,0 +1,9 @@
+const lightbox = GLightbox({
+    //'href': './images/videos/rickroll.mp4',
+    'href': 'https://www.youtube.com/watch?v=wLNp3kgBuuQ',
+    'type': 'video',
+    'source': 'local',//vimeo, youtube or local
+    'width': 900,
+    'autoPlayVideos': true,
+    'autoplay': true
+  });
